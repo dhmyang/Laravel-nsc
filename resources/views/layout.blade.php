ef62f2ba6711ecc5c0ea2537cf66030ab9b5f185
dhmyang <55231982+dhmyang@users.noreply.github.com>
2020-11-27 10:11:48 +0100

    First push

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <title>Titel hier</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="Mike Yang">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    
+    <body>
+        @yield(navbar)
+        @yield("content")
+
+    </body>
+    <script src="./script.js"></script>
+</html>
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,6 @@
+@extends("layout")
+
+@section ("content")
+
+<p>random test</p>
+@endsection
